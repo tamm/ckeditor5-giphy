e94e83e2da8a45c7ca3be86fb672981b9c6bb409
Tamm Sjödin <tsjoedin@squiz.net>
2020-09-09 13:55:51 +1000

    Add ckeditor5-giphy code to repo

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # ckeditor5-giphy
+
+This is a POC of integration between CK Editor 5 and the Giphy API.
+
+Note: Giphy suggests using their SDK rather than API, and the included key is a rate limited beta key.
+
+This also demonstrates how you can use SweetAlert2 as a plugin modal replacement for CK Editor 5, since it doens't come OOTB. 
+As well as selecting something from a result of an AJAX request in the modal.
+
+## Getting set up
+
+Install and import as 
+```
+import 'ckeditor5-giphy';
+```
+
+Then add to CK Editor 5 config as
+
+```
+config={ {
+    plugins: [ Giphy, Image ],
+    toolbar: [ 'giphy']
+} }
+```
+
+Visit https://developers.giphy.com/docs/api#quick-start-guide
+to get an API key.
+
+Then add your key to the global variable for config.
+You can also change a couple of other settings:
+
+```
+const giphyOptions = {
+  apiBase: 'https://api.giphy.com/v1',
+  apiKey: 'YOUR-KEY-HERE', // https://developers.giphy.com/docs/api#quick-start-guide
+}
+```
+
+## License
+
+As CK Editor 5 is GPL, this is GPL. 
+Please consider this if you reuse the code in any way.
---
+++ src/ckeditor-giphy.js
@@ -0,0 +1,105 @@
+/*global giphyOptions*/
+
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import Swal from 'sweetalert2';
+import './ckeditor-giphy.css';
+
+import imageIcon from './giphy.svg';
+
+const giphyOptionsDefault = {
+  apiBase: 'https://api.giphy.com/v1',
+  apiKey: 'lBj9wdWfSYDszxMdstVhL6ODrxDLh8Xr', // https://developers.giphy.com/docs/api#quick-start-guide
+}
+  
+export default class Giphy extends Plugin {
+  init() {
+    const editor = this.editor;
+    /* es-lint-disable no-undef */
+    const giphyOptionsCombined = typeof giphyOptions !== 'undefined' ? {...giphyOptionsDefault, ...giphyOptions} : {...giphyOptionsDefault};
+
+    editor.ui.componentFactory.add( 'giphy', locale => {
+      const view = new ButtonView( locale );
+
+      view.set( {
+        label: 'GIPHY',
+        icon: imageIcon,
+        tooltip: true
+      } );
+
+      // Callback executed once the image is clicked.
+      view.on( 'execute', () => {
+        Swal.fire({
+          customClass: {
+            container: 'ckeditor-giphy-modal',
+          },
+          title: 'Search for a gif on GIPHY',
+          input: 'text',
+          inputAttributes: {
+            autocapitalize: 'off'
+          },
+          inputValidator: (value) => {
+            if (value.length < giphyOptionsCombined.minimumCharactersToSearch) {
+              return `Enter at least ${giphyOptionsCombined.minimumCharactersToSearch} characters!`
+            }
+          },
+          showCancelButton: true,
+          confirmButtonText: 'Search',
+          showLoaderOnConfirm: true,
+          preConfirm: (searchInput) => {
+            const encodedSearch = encodeURIComponent(searchInput);
+            const giphyRequest = fetch(`${giphyOptionsCombined.apiBase}/gifs/search?q=${encodedSearch}&api_key=${giphyOptionsCombined.apiKey}&limit=5`)
+              .then(response => response.json())
+              .then((response) => {
+                if (response.data && typeof response.data === 'object') {
+                  return response.data.map(item => {return { value: item.images.original.url, icon: `<img src="${item.images.original.url}" title="Select ${item.title}" alt="Select ${item.title}"/>`}});
+                }
+              }).catch(error => {
+                Swal.showValidationMessage(
+                  `Request failed: ${error}`
+                )
+              })
+            return giphyRequest;
+          },
+          allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+          const inputOptions = new Promise((resolve) => {
+            resolve(result.value.reduce((reducedItems, item) => {
+              reducedItems[item.value] = item.icon;
+              return reducedItems;
+            }, {}))
+          })
+          
+          if (result.isConfirmed) {
+            return Swal.fire({
+              customClass: {
+                container: 'ckeditor-giphy-modal',
+              },
+              title: 'Select result',
+              input: 'radio',
+              inputOptions: inputOptions,
+              inputValidator: (value) => {
+                if (!value) {
+                  return 'You need to choose something!'
+                }
+              },
+              showCancelButton: true,
+            })
+          }
+        }).then((result) => {
+          editor.model.change( writer => {
+            const imageElement = writer.createElement( 'image', {
+              src: result.value
+            } );
+  
+            // Insert the image in the current selection location.
+            editor.model.insertContent( imageElement, editor.model.document.selection );
+          } );
+        })
+      } );
+
+      return view;
+    } );
+  }
+}
+
